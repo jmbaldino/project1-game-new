34382a00f2bf5f26d6a71d0cb29b160ecb30d84d
José Baldino <josemanuelbaldino@gmail.com>
2025-04-15 21:17:51 +0100

    add player rotation and restrict its movement to the board area

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 class Player {
     constructor() {
         this.spaceship = document.getElementById("spaceship");
+        this.board = document.getElementById("board");
         this.x = 0;
         this.y = 0;
         this.speed = 3;
         this.direction = null;
+        this.rotation = 0;
 
         this.spaceship.style.position = "absolute";
         this.spaceship.style.left = `${this.x}px`;
         this.spaceship.style.top = `${this.y}px`;
+        this.spaceship.style.transform = `rotate(${this.rotation}deg)`;
 
         this.initControls();
         this.move();
@@ -19,34 +22,46 @@ class Player {
             switch (event.key) {
                 case "ArrowLeft":
                     this.direction = "left";
+                    this.rotation = -90;
                     break;
                 case "ArrowRight":
                     this.direction = "right";
+                    this.rotation = 90;
                     break;
                 case "ArrowUp":
                     this.direction = "up";
+                    this.rotation = 0;
                     break;
                 case "ArrowDown":
                     this.direction = "down";
+                    this.rotation = 180;
                     break;
             }
+
+            this.spaceship.style.transform = `rotate(${this.rotation}deg)`;
         });
     }
 
     move() {
+        const boardRect = this.board.getBoundingClientRect();
+        const shipRect = this.spaceship.getBoundingClientRect();
+
+        const maxX = this.board.clientWidth - this.spaceship.offsetWidth;
+        const maxY = this.board.clientHeight - this.spaceship.offsetHeight;
+
         if (this.direction) {
             switch (this.direction) {
                 case "left":
-                    this.x -= this.speed;
+                    if (this.x > 0) this.x -= this.speed;
                     break;
                 case "right":
-                    this.x += this.speed;
+                    if (this.x < maxX) this.x += this.speed;
                     break;
                 case "up":
-                    this.y -= this.speed;
+                    if (this.y > 0) this.y -= this.speed;
                     break;
                 case "down":
-                    this.y += this.speed;
+                    if (this.y < maxY) this.y += this.speed;
                     break;
             }
 
@@ -61,4 +76,6 @@ class Player {
 window.onload = () => {
     const player = new Player();
 };
+
+
 
